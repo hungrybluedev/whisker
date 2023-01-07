3abafb2464e69f17d9f382efaf9d7657c52b6797
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-01-07 19:44:58 +0000

    Support boolean sections

--- whisker/models.v
+++ whisker/models.v
@@ -2,6 +2,7 @@ module whisker
 
 import strings
 import encoding.html
+import datatypes
 
 pub type DataModel = []DataModel | bool | map[string]DataModel | string
 
@@ -26,17 +27,29 @@ pub fn new_template(input string) !WhiskerTemplate {
 	}
 }
 
+enum SectionType {
+	boolean
+	list
+	map
+}
+
+struct Section {
+	name         string
+	section_type SectionType
+}
+
 pub fn load_template(file string) !WhiskerTemplate {
 	return error('Not implemented yet.')
 }
 
 pub fn (template WhiskerTemplate) run(context DataModel) !string {
-	dump(template.tokens)
 	mut index := 0
 	mut output := strings.new_builder(256)
 
 	mut data_stack := DataStack{}
 	data_stack.push(context)
+
+	mut sections := datatypes.Stack[Section]{}
 
 	for index < template.tokens.len {
 		token := template.tokens[index]
@@ -55,14 +68,42 @@ pub fn (template WhiskerTemplate) run(context DataModel) !string {
 				index++
 			}
 			.positive_section {
-				index++
+				switch := data_stack.query_boolean_section(token.content)!
+				if !switch {
+					for template.tokens[index].token_type != .close_section
+						&& template.tokens[index].content != token.content {
+						index++
 
-				// TODO
+						if index >= template.tokens.len {
+							return error('Could not find section closing tag for ${token.content}')
+						}
+					}
+				} else {
+					sections.push(Section{
+						name: token.content
+						section_type: .boolean
+					})
+					index++
+				}
 			}
 			.negative_section {
-				index++
+				switch := data_stack.query_boolean_section(token.content)!
+				if switch {
+					for template.tokens[index].token_type != .close_section
+					&& template.tokens[index].content != token.content {
+						index++
 
-				// TODO
+						if index >= template.tokens.len {
+							return error('Could not find section closing tag for ${token.content}')
+						}
+					}
+				} else {
+					sections.push(Section{
+						name: token.content
+						section_type: .boolean
+					})
+					index++
+				}
 			}
 			.map_section {
 				index++
@@ -75,9 +116,14 @@ pub fn (template WhiskerTemplate) run(context DataModel) !string {
 				// TODO
 			}
 			.close_section {
+				if sections.is_empty() {
+					return error('Found a stray closing tag.')
+				}
+				last_section := sections.pop()!
+				if last_section.name != token.content {
+					return error('Expected to close ${last_section.name}, closed ${token.content} instead.')
+				}
 				index++
-
-				// TODO
 			}
 		}
 	}
