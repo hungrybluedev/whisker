3e1957cdfc1e07b2887fafadbb91440399de3592
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-03-30 13:51:06 +0100

    Simplify API support files

--- whisker/models.v
+++ whisker/models.v
@@ -1,24 +1,32 @@
 module whisker
 
+import os
+
 pub struct WhiskerTemplate {
 pub:
 	tokens   []Token
 	partials map[string][]Token
 }
 
-pub fn new_template(input string, partials map[string]string) !WhiskerTemplate {
-	if input.len == 0 {
+[params]
+pub struct TemplateConfig {
+	input    string            [required]
+	partials map[string]string = {}
+}
+
+pub fn new_template(config TemplateConfig) !WhiskerTemplate {
+	if config.input.len == 0 {
 		return WhiskerTemplate{}
 	}
 
 	mut tokenized_partials := map[string][]Token{}
 
-	for label, partial in partials {
+	for label, partial in config.partials {
 		tokenized_partials[label] = tokenize(partial)!
 	}
 
 	return WhiskerTemplate{
-		tokens: tokenize(input)!
+		tokens: tokenize(config.input)!
 		partials: tokenized_partials
 	}
 }
@@ -28,6 +36,13 @@ struct Section {
 	contexts []DataModel
 }
 
-pub fn load_template(file string) !WhiskerTemplate {
-	return error('Not implemented yet.')
+pub fn load_template(config TemplateConfig) !WhiskerTemplate {
+	input := os.read_file(config.input)!
+
+	mut partial_contents := map[string]string{}
+	for partial_name, path in config.partials {
+		partial_contents[partial_name] = os.read_file(path)!
+	}
+
+	return new_template(input: input, partials: partial_contents)
 }
