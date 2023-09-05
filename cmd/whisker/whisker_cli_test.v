757ce8bc28a8558d2cf89291a44c0491bde752f4
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-09-05 06:28:17 +0100

    fix path to executable on Windows

--- cmd/whisker/whisker_cli_test.v
+++ cmd/whisker/whisker_cli_test.v
@@ -5,10 +5,15 @@ import net.html
 
 fn test_whisker_cli() {
 	cases := os.ls('spec/template_files/html/cases')!
+
+	binary_dir := os.join_path('cmd', 'bin')
+	binary_path := os.join_path(binary_dir, 'whisker')
+
 	for case in cases {
-		os.mkdir('cmd/bin') or {}
-		os.execute_or_panic('${os.quoted_path(@VEXE)} cmd/whisker -o cmd/bin/whisker')
-		result := os.execute_or_panic('cmd/bin/whisker' +
+		os.rmdir(binary_dir) or {}
+		os.mkdir(binary_dir) or {}
+		os.execute_or_panic('${os.quoted_path(@VEXE)} ${os.join_path('cmd', 'whisker')} -o ${binary_path}')
+		result := os.execute_or_panic(binary_path +
 			' --input spec/template_files/html/base.wskr.html' +
 			' --data spec/template_files/html/cases/${case}/data.wskr.json' +
 			' --partials head:spec/template_files/html/head.wskr.html,' +
